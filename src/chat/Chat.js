999aebf47e4acd79087da26a552e002541d56485
shamin.asfaq@gmail.com <shamin.asfaq@gmail.com>
2020-07-17 03:36:04 +0600

    Chat box can be closed and opened.

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import MessageList from './MessageList';
 import ScrollToBottom from 'react-scroll-to-bottom';
 
@@ -10,41 +10,78 @@ const Chat = () => {
     };
 
     let onCloseChatBox = () => {
-        console.log('Toss a coin to your witcher');
+        let foundDiv = document.getElementsByClassName('chat-window')
+
+        if(foundDiv[0]) {
+            let foundVisibility = document.getElementsByClassName('chat-window')[0].style.visibility;
+            updateVisible(foundVisibility==='hidden'?true:false);
+        } else {
+            let foundVisibility = hiddenElement.style.visibility;
+            updateVisible(foundVisibility==='hidden'?false:true);
+        }
     }
 
     let isBlocked = false;
 
-    useEffect(() => {});
+    let [visible, updateVisible] = useState(true);
+    let [hiddenElement, updateHiddenElement] = useState([]);
+
+    useEffect(() => {
+        let foundDiv = document.getElementsByClassName('chat-window')
+
+        if(foundDiv[0]) {
+            let foundVisibility = document.getElementsByClassName('chat-window')[0].style.visibility;
+            updateVisible(foundVisibility==='hidden'?false:true);
+            updateHiddenElement(foundDiv[0]);
+        }
+    });
 
     return(
-        <div className='chat-window'>
-            <div className='chat-box-head-wannabe'>
-                <div className='chat-box-head'>
-                    #general
-                    <button onClick={onCloseChatBox} className='close-button'>
-                        close
-                    </button>
-                </div>
-            </div>
-            <MessageList/>
+        <div>
             {
-                !isBlocked &&
-                <div className="ui big icon input chat-input">
-                    <textarea className='chat-input-text-area' rows='2'></textarea>
-                    <button 
-                        className="send-button" 
-                        type="submit"
-                        onClick={onSendMessage}
-                    >
-                    Send
-                    </button>
+                visible &&
+                <div className='chat-window'>
+                    <div className='chat-box-head-wannabe'>
+                        <div className='chat-box-head'>
+                            #general
+                            <button onClick={onCloseChatBox} className='close-button'>
+                                close
+                            </button>
+                        </div>
+                    </div>
+                    <MessageList/>
+                    {
+                        !isBlocked &&
+                        <div className="ui big icon input chat-input">
+                            <textarea className='chat-input-text-area' rows='2'></textarea>
+                            <button 
+                                className="send-button" 
+                                type="submit"
+                                onClick={onSendMessage}
+                            >
+                            Send
+                            </button>
+                        </div>
+                    }
+                    {
+                        isBlocked &&
+                        <div style={{textAlign:'center', color: 'red', paddingTop: '7%'}}>
+                            Unfortunately, you have been blocked.
+                        </div>
+                    }
                 </div>
             }
             {
-                isBlocked &&
-                <div style={{textAlign:'center', color: 'red', paddingTop: '7%'}}>
-                    Unfortunately, you have been blocked.
+                !visible &&
+                <div className='chat-window-closed'>
+                    <div className='chat-box-head-wannabe'>
+                        <div className='chat-box-head'>
+                            #general
+                            <button onClick={onCloseChatBox} className='close-button'>
+                                open
+                            </button>
+                        </div>
+                    </div>
                 </div>
             }
         </div>

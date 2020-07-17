47976d256540da01da569a40909ba7ae0ea1e94c
shamin.asfaq@gmail.com <shamin.asfaq@gmail.com>
2020-07-17 21:56:57 +0600

    Focus on TextArew if clicked inside MessageList has been disabled. It's prohibiting the user from copying text from a message.

--- src/chat/MessageList.js
+++ src/chat/MessageList.js
@@ -5,7 +5,11 @@ const MessageList = (props) => {
     let availableMessages = props.availableMessages;
 
     let focusOnTextArea = () => {
-        document.getElementsByClassName('chat-input-text-area')[0].focus();
+        /**
+         * To make the TextArea selected if clicked inside message list.
+         * Buggy, becasue it doesn't allow you to copy any text from a message.
+         */
+        // document.getElementsByClassName('chat-input-text-area')[0].focus();
     }
 
     return (

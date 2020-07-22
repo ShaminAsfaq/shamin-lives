e123a4b28886a9a09e8d2f47d600355b9ba49437
shamin.asfaq@gmail.com <shamin.asfaq@gmail.com>
2020-07-23 01:45:26 +0600

    Search redditor working. But not good looking, though.

--- src/components/App.js
+++ src/components/App.js
@@ -52,7 +52,9 @@ const App = () => {
                     // <Spotify />
                 }
             </div>
-            {/* <Chat/> */}
+            {
+                <Chat/>
+            }
              {
                 // <Conference/>
              }
